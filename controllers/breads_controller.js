8b2a63df1fd8d62fa2df68a196b1454abc3a2801
Elijah Oquendo <epoquendo2@gmail.com>
2024-05-01 20:09:09 -0400

    created .findByIdAndUpdate Helper Method + a Seed Route with .insertMany

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,7 +2,6 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-// INDEX
 breads.get('/', (req, res) => {
     Bread.find()
         .then(foundBreads => {
@@ -13,11 +12,19 @@ breads.get('/', (req, res) => {
         })
 })
 
-
-
 // NEW
 breads.get('/new', (req, res) => {
     res.render('new')
+})
+
+breads.get('/:id', (req, res) => {
+    console.log(req.body);
+    Bread.findById(req.params.id)
+        .then(foundBread => {
+            res.render('show', {
+                bread: foundBread
+            })
+        })
 })
 
 breads.get('/:id', (req, res) => {
@@ -46,33 +53,67 @@ breads.post('/', (req, res) => {
     res.redirect('/breads')
 })
 
-
-
-// // DELETE
-// breads.delete('/:indexArray', (req, res) => {
-//     Bread.splice(req.params.indexArray, 1)
-//     res.status(303).redirect('/breads')
-// })
+breads.get('/:id/edit', (req, res) => {
+    Bread.findById(req.params.id)
+        .then(foundBread => {
+            res.render('edit', {
+                bread: foundBread
+            })
+        })
+        .catch(err => {
+            res.send('404')
+        })
+})
 
 // UPDATE
-// breads.put('/:arrayIndex', (req, res) => {
-//     console.log(req.body)
-//     if (req.body.hasGluten === 'on') {
-//         req.body.hasGluten = true
-//     } else {
-//         req.body.hasGluten = false
-//     }
-//     Bread[req.params.arrayIndex] = req.body
-//     res.redirect(`/breads/${req.params.arrayIndex}`)
-// })
+breads.put('/:id', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(updatedBread => {
+            console.log(updatedBread)
+            res.redirect(`/breads/${req.params.id}`)
+        })
+})
 
-// EDIT
-// breads.get('/:indexArray/edit', (req, res) => {
-//     res.render('edit', {
-//         bread: Bread[req.params.indexArray],
-//         index: req.params.indexArray
-//     })
-// })
+// DELETE
+breads.delete('/:id', (req, res) => {
+    Bread.findByIdAndDelete(req.params.id)
+        .then(deletedBread => {
+            console.log(deletedBread);
+            res.status(303).redirect('/breads')
+        })
+})
 
+// SEED
+breads.get('/data/seed', (req, res) => {
+    Bread.insertMany([
+        {
+            name: 'Rye',
+            hasGluten: true,
+            image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+        },
+        {
+            name: 'French',
+            hasGluten: true,
+            image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+        },
+        {
+            name: 'Gluten Free',
+            hasGluten: false,
+            image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+        },
+        {
+            name: 'Pumpernickel',
+            hasGluten: true,
+            image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+        }
+    ]).then(createdBreads => {
+        res.redirect('/breads')
+    })
+})
 
 module.exports = breads
